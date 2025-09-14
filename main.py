b8d5beed3cb95c7f208edea937d514d7ff50ae8e
soufianesys710 <soufianenidam7@gmail.com>
2025-09-14 17:46:00 +0100

    delete main

--- main.py
+++
@@ -1,5 +0,0 @@
-from seconddraftml.regression import MetaRandomForestRegressor
-from sklearn.utils.estimator_checks import check_estimator
-
-check_estimator(MetaRandomForestRegressor())
-print("True")
